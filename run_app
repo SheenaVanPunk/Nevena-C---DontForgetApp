50af442ea2512adbae3e643c0e1865f378b95581
Nevena Cukucan <ljubicpantic@gmail.com>
2020-06-11 17:55:56 -0400

    added run_app file

---
+++ run_app
@@ -0,0 +1,6 @@
+from ProgramFlow import ProgramFlow
+
+program = ProgramFlow()
+
+if __name__ == '__main__':
+    program.main()
--- tests/ProgramFlowTest.py
+++ tests/ProgramFlowTest.py
@@ -4,6 +4,5 @@ from ProgramFlow import ProgramFlow
 program1 = ProgramFlow()
 program1.main()
 
-# program1.authenticate_user()
-# program1.show_user_menu(user)
+
 
